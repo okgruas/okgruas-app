5d754104bf6c226ff80798f627c9d56183238a16
okgruas <despachoalec@gmail.com>
2026-05-02 01:41:18 -0600

    Update app_gruas.py

--- app_gruas.py
+++ app_gruas.py
@@ -66,7 +66,7 @@ if menu == "📱 Cotizador":
         total = banderazo + (distancia * costo_km) if distancia > 0 else banderazo
         
         st.info(f"Costo base (Banderazo): ${banderazo} MXN")
-        if distancia > 0:
+        st.info(f"Costo base (km): ${km} MXN")        if distancia > 0:
             st.success(f"Total Estimado: ${total} MXN")
         
         btn_enviar = st.form_submit_button("📩 SOLICITAR GRÚA POR WHATSAPP")
